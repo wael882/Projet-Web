dc956a053da2165d4b166aab1350dd0c53240ee5
Wael <droussiwael@gmail.com>
2026-03-18 10:29:23 +0100

    feat: logout et fix base_url global dans twig

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ use App\Controllers\PageController;
 // Twig
 $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates');
 $twig = new \Twig\Environment($loader, ['cache' => dirname(__DIR__) . '/cache/twig', 'auto_reload' => true]);
+$twig->addGlobal('base_url', '/');
 
 // Test connexion BDD
 $db = Database::getInstance()->getPdo();

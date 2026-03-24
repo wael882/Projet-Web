f73f1372fd96fa08e138d069a88356d6a84d79fa
mathduvivier <mathduvivie@gmail.com>
2026-03-24 22:59:32 +0100

    config php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-//variable "d'environnement" a utiliser plus tard dans le code 
+// variable "d'environnement" a utiliser plus tard dans le code 
 
-define('BASE_URL', 'https://cesi-site.local/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'Admin1234!');
+define('BASE_URL', 'http://cesi-site.local:8000/'); // J'ai corrigé le nom et ajouté le port pour tes futurs liens !
+define('DB_HOST', '172.19.128.1');
+define('DB_USER', 'cesi_user'); // On utilise le nouvel utilisateur !
+define('DB_PASSWORD', 'Root1234!');
 define('DB_NAME', 'cesi_db');
 define('ROOT_PATH', dirname(__DIR__));
 define('UPLOAD_PATH', ROOT_PATH . '/public/uploads');

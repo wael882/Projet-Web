26334a1c585ce1f7d4ab029225063202054bf765
Wael <droussiwael@gmail.com>
2026-03-26 09:24:57 +0100

    fix: erreur 404 lors de la deconexion

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 
 //variable "d'environnement" a utiliser plus tard dans le code 
 
-define('BASE_URL', 'https://cesi-site.local/');
-define('DB_HOST', '172.19.128.1');
+define('BASE_URL', 'https://stagio.local/');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'Admin123!');
 define('DB_NAME', 'cesi_db');
